13d95157273b26f9c1145efeb6d822215b82441d
Rui Reogo <149220283+ruingl@users.noreply.github.com>
2024-02-13 21:20:38 +0800

    Create roll.js

---
+++ akhiro/cmds/roll.js
@@ -0,0 +1,13 @@
+module.exports = {
+  config: {
+    name: "roll",
+    description: "Roll a virtual six-sided die",
+    usage: "roll",
+    author: "Rui",
+    aliases: ["dice"],
+  },
+  onRun: ({ api, event }) => {
+    const result = Math.floor(Math.random() * 6) + 1;
+    api.sendMessage(`🎲 You rolled a ${result}!`, event.threadID, event.messageID);
+  },
+};
