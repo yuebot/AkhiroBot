c767f8e18848da1c71f2cfe4ba19f2904f59bca8
ruingl <149220283+ruingl@users.noreply.github.com>
2024-02-21 13:32:20 +0000

    hehehe

--- index.js
+++ index.js
@@ -156,7 +156,19 @@ function initializeBot() {
 
               if (event.body && event.body.toLowerCase() === "prefix") {
                 api.sendMessage(
-                  `My prefix is: \`${global.AkhiroBot.botPrefix}\``,
+                  fonts.applyFonts(`▄▀█ █▄▀ █░█ █ █▀█ █▀█
+█▀█ █░█ █▀█ █ █▀▄ █▄█
+━━━━━━━━━━━━━━━━━━━
+Hello there User, Im AkhiroBot. My Prefix Is This [ ${global.AkhiroBot.botPrefix} ]. I Hope You Will Enjoy Using Me, Have Fun. Thanks!
+
+AkhiroBot Is A Collab Bot File Project Made By AkhiroDev, LiANE And Yu3Bot, So Please If You Managed To Find This File, Please Don't Abuse This Bot File Project Of Our's.
+
+DEVELOPERS OF AKHIROBOT:
+- YueBot ( Rui Reogo )
+- AkhiroDev ( Francis Loyd Raval )
+- LiANE ( Liane Cagara )
+
+YSD: AkhiroBot`, "sans"),
                   event.threadID,
                   event.messageID,
                 );
